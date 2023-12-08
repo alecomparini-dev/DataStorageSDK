31526b5d367a81af00b931ca76c07786b19b4e38
Comparini <alecomparini.dev@gmail.com>
2023-12-08 18:36:46 -0300

    initiating realm and coredata storage

---
+++ Sources/Detail/DBStorageProvider/CoreData/CoreDataProvider.swift
@@ -0,0 +1,12 @@
+//  Created by Alessandro Comparini on 08/12/23.
+//
+
+import Foundation
+
+import DataStorageInterfaces
+
+public class CoreDataProvider: DataStorageProviderStrategy {
+    
+    
+    
+}

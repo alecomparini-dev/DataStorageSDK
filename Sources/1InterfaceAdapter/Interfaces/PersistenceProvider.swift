69aa260dd4832eec296b896c3b38eb18cd78701a
Comparini <alecomparini.dev@gmail.com>
2023-12-11 22:01:40 -0300

    fix

--- Sources/1InterfaceAdapter/Interfaces/PersistenceProvider.swift
+++ Sources/1InterfaceAdapter/Interfaces/PersistenceProvider.swift
@@ -9,10 +9,10 @@ import CoreData
 public protocol PersistenceProvider{
     func create<T>(_ object: T) async throws -> T?
     func fetch<T>() async throws -> [T]
-//    func fetch(limit: Int) async throws -> T?
+//    func fetch(limit: Int) async throws -> [T]
 //    func fetchCount() async throws -> Int
 //    func fetchById(_ id: String) async throws -> T?
-//    func findByColumn<D>(column: String, value: D) async throws -> T?
+//    func findByColumn<D>(column: String, value: D) async throws -> [T]
 //    func delete(_ object: T) async throws
 //    func update(_ object: T) async throws -> T
 

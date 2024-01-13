57598ed5c6949da72642b9914812966952314514
Comparini <alecomparini.dev@gmail.com>
2024-01-13 16:15:19 -0300

    attempt [T] yet

--- Sources/1InterfaceAdapter/Interfaces/PersistenceProvider.swift
+++ Sources/1InterfaceAdapter/Interfaces/PersistenceProvider.swift
@@ -7,12 +7,12 @@ import Foundation
 public protocol PersistenceProvider{
     func create<T>(_ object: T) async throws -> T?
     
-    func fetch<T>() async throws -> T?
+    func fetch<T>() async throws -> [T]
     func fetch<T>(limit: Int) async throws -> [T]
     func fetchCount() async throws -> Int
     
-    func findBy<T>(_ id: String) async throws -> T?
-    func findBy<T,V>(column: String, value: V) async throws -> T?
+    func findBy<T>(_ id: String) async throws -> [T]
+    func findBy<T,V>(column: String, value: V) async throws -> [T]
 
     //    func delete(_ object: T) async throws
 //    func update(_ object: T) async throws -> T
